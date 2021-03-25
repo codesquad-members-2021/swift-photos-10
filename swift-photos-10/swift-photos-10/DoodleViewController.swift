6f04a4a156a5fc740bfb92524afb8835bb397fdd
BMO <b38238496@gmail.com>
2021-03-25 14:50:28 +0900

    Navigation Embed

---
+++ swift-photos-10/swift-photos-10/DoodleViewController.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+import UIKit
+
+class DoodleViewController : UICollectionViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Doodles"
+    }
+    
+}
--- swift-photos-10/swift-photos-10/ViewController.swift
+++ swift-photos-10/swift-photos-10/ViewController.swift
@@ -4,20 +4,28 @@ import Photos
 class ViewController: UIViewController {
     
     @IBOutlet weak var photoCollectionView: UICollectionView!
+    @IBOutlet weak var addButton: UIBarButtonItem!
     
     private var photoAlbum = PhotoLibrary()
-    
+    private let app = UIApplication.shared.delegate as! AppDelegate
+
     override func viewDidLoad() {
         super.viewDidLoad()
         PHPhotoLibrary.shared().register(self)
+        
     }
+
+    @IBAction func addButtonPressed(_ sender: Any) {
+        self.present(app.navigationController, animated: true, completion: nil)
+    }
+    
 }
 
 extension ViewController : UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.photoAlbum.allPhotos.count
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
         self.photoAlbum.requestImage(cell: cell, indexPath: indexPath)
